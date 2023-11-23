f50aabbdaffff2727ecfa1f1474a4d1f763a7fea
barryarganezaa <argnz158cyber@gmail.com>
2023-11-23 08:43:55 +0700

    db customer

---
+++ database/migrations/2023_11_16_095358_create_customer.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id('id_customer');
+            $table->string('nama_customer');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->integer('billing');
+            $table->integer('no_telp');
+            $table->rememberToken();
+            $table->timestamps('create_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+};
