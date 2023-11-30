617beea63fa79573afd094102376e1a1b0ac7d7c
barryarganezaa <argnz158cyber@gmail.com>
2023-11-30 11:52:11 +0700

    border excel

--- app/Exports/CustomerExport.php
+++ app/Exports/CustomerExport.php
@@ -5,8 +5,13 @@ namespace App\Exports;
 use App\Models\Customer;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class CustomerExport implements FromCollection, WithHeadings
+class CustomerExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents, WithStyles
 {
     public function collection()
     {
@@ -31,6 +36,30 @@ class CustomerExport implements FromCollection, WithHeadings
             'Billing',
             'No Telp',
             'Create Date',
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        // Menambahkan border
+        $styleArray = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'],
+                ],
+            ],
+        ];
+
+        $sheet->getStyle('A1:' . $sheet->getHighestColumn() . $sheet->getHighestRow())->applyFromArray($styleArray);
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                // Implementasi kustom setelah lembar telah dibuat
+            },
         ];
     }
 }
