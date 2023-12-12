0de2e4ce3cc5b317d4cfb138c208b2dd88fca5eb
ghifariwandanaa <ghifariwandana@gmail.com>
2023-12-12 22:06:07 +0700

    penambahan data warnet untuk customer

--- resources/views/dashboard/dataWarnet.blade.php
+++ resources/views/dashboard/dataWarnet.blade.php
@@ -1,5 +1,19 @@
 @extends('dashboard.dashboardLayout')
 
 @section('konten2')
-
+<div class="content">
+    <h2><strong>Data Warnet</strong></h2>
+    <div class="row">
+        @foreach ($warnets as $warnet)
+            <div class="col-sm-6 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $warnet->nama_warnet }}</h5>
+                        <p class="card-text">{{ $warnet->alamat }}</p>
+                        <a href="#" class="btn btn-primary">Lihat</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
